8d70c690dd73772b2fdd004a3b9859f6a9261673
kie4280 <kie4280ann@gmail.com>
2021-11-14 08:40:13 +0800

    parallelize pagerank, pass grader

--- HW3/part2/page_rank/page_rank.cpp
+++ HW3/part2/page_rank/page_rank.cpp
@@ -22,7 +22,6 @@ void pageRank(Graph g, double *solution, double damping, double convergence) {
 
   int numNodes = num_nodes(g);
   double *score_old = new double[numNodes];
-  // double *score_new = new double[numNodes];
   double equal_prob = 1.0 / numNodes;
   for (int i = 0; i < numNodes; ++i) {
     solution[i] = equal_prob;
@@ -43,46 +42,52 @@ void pageRank(Graph g, double *solution, double damping, double convergence) {
   bool converged = false;
   while (!converged) {
     double no_out = 0.0;
-    for (int i = 0; i < numNodes; ++i) {
-      int o = outgoing_size(g, i);
-      if (o == 0) {
-        no_out += damping * score_old[i] / numNodes;
-      }
-    }
-
-    for (int i = 0; i < numNodes; ++i) {
-      const Vertex *begin = incoming_begin(g, i);
-      const Vertex *end = incoming_end(g, i);
-      double ns = 0.0;
-      for (const Vertex *v = begin; v != end; ++v) {
-        int edges = outgoing_size(g, *v);
-        ns += score_old[*v] / edges;
-      }
-
-      solution[i] = damping * ns + (1.0 - damping) / numNodes + no_out;
-    }
-
-    // compute score_new[vi] for all nodes vi:
-    // score_new[vi] =
-    //     sum over all nodes vj reachable from incoming edges{
-    //         score_old[vj] / number of edges leaving vj}
-    // score_new[vi] =
-    //         (damping * score_new[vi]) + (1.0 - damping) / numNodes;
-
-    // score_new[vi] +=
-    //     sum over all nodes v in graph with no outgoing edges{
-    //         damping * score_old[v] / numNodes}
-
-    // compute how much per-node scores have changed
-    // quit once algorithm has converged
     double global_diff = 0.0;
-    for (int i = 0; i < numNodes; ++i) {
-      global_diff += abs(solution[i] - score_old[i]);
-      score_old[i] = solution[i];
+
+#pragma omp parallel
+    {
+#pragma omp for reduction(+ : no_out)
+      for (int i = 0; i < numNodes; ++i) {
+        int o = outgoing_size(g, i);
+        if (o == 0) {
+          no_out += damping * score_old[i] / numNodes;
+        }
+      }
+#pragma omp for
+      for (int i = 0; i < numNodes; ++i) {
+        const Vertex *begin = incoming_begin(g, i);
+        const Vertex *end = incoming_end(g, i);
+        double ns = 0.0;
+        for (const Vertex *v = begin; v != end; ++v) {
+          int edges = outgoing_size(g, *v);
+          ns += score_old[*v] / edges;
+        }
+
+        solution[i] = damping * ns + (1.0 - damping) / numNodes + no_out;
+      }
+
+// compute score_new[vi] for all nodes vi:
+// score_new[vi] =
+//     sum over all nodes vj reachable from incoming edges{
+//         score_old[vj] / number of edges leaving vj}
+// score_new[vi] =
+//         (damping * score_new[vi]) + (1.0 - damping) / numNodes;
+
+// score_new[vi] +=
+//     sum over all nodes v in graph with no outgoing edges{
+//         damping * score_old[v] / numNodes}
+
+// compute how much per-node scores have changed
+// quit once algorithm has converged
+#pragma omp for reduction(+ : global_diff)
+      for (int i = 0; i < numNodes; ++i) {
+        global_diff += abs(solution[i] - score_old[i]);
+        score_old[i] = solution[i];
+      }
+#pragma omp single
+      { converged = (global_diff < convergence); }
     }
-    converged = (global_diff < convergence);
   }
 
   delete[] score_old;
-  // delete[] score_new;
 }
